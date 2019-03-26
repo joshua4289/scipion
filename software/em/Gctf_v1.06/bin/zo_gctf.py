389749ef4668f4b464c4c30f1a84c49ebd0ac71d
Joshua Lobo <joshua.jude.lobo@gmail.com>
2019-03-26 13:23:06 +0000

    get USER from env to set tmplog

--- software/em/Gctf_v1.06/bin/zo_gctf.py
+++ software/em/Gctf_v1.06/bin/zo_gctf.py
@@ -34,12 +34,7 @@ def run_zocalo_gctf():
 
     out = p1.communicate()[0]
 
-    # cwd/Runs/0000122_ProcGctf/blah/blah/ctfEstimation.txt
 
-    # /home/jtq89441/workspace/zocalo_test/motioncor2_launcher/testing/share_local/Runs/000122_ProtGctf/extra/
-    # GridSquare_404_Data_FoilHole_5419591_Data_5417705_5417706_20180306_2106-140228_aligned_mic/ctfEstimation.txt
-
-    # print("***************OUT is "+str(out))
     return out
 
     # TODO:1: get the scipion directory from the recipie from the ['cwd'] instead of the command
@@ -47,7 +42,12 @@ def run_zocalo_gctf():
 
 
 if __name__ == '__main__':
-    with open("/dls/tmp/zo_gctf_log.tmp", 'w') as tmplog:
+    import os
+
+    user = os.environ.get('USER')
+    tmp_log_file = os.path.join('/dls/tmp/', user, 'zo_gctf_log.tmp')
+
+    with open(tmp_log_file, 'w') as tmplog:
 
         tmplog.write("WE ARE STARTING ZO_GCTF\n")
         tmplog.write("CWD is %s\n" % (os.getcwd()))
@@ -69,10 +69,10 @@ if __name__ == '__main__':
         tmplog.write("EXITING!!\n")
         # remove the extra done file when program1
 
-    sys.exit(0)
+
     if os.path.exits(extra_done_file):
         os.remove(extra_done_file)
-
+        sys.exit(0)
 
 
 
