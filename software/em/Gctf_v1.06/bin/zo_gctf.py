bf8a867ce1c46ae5d459ef49ef27603b806e6257
Joshua Lobo <joshua.jude.lobo@gmail.com>
2019-05-23 11:25:17 +0100

    removed dependency on /dls/tmp/ writing a log file

--- software/em/Gctf_v1.06/bin/zo_gctf.py
+++ software/em/Gctf_v1.06/bin/zo_gctf.py
@@ -29,51 +29,22 @@ def run_zocalo_gctf():
     p1 = Popen(cmd + " ".join(args), shell=True, stdout=subprocess.PIPE)
     p1.wait()
     import time
-    time.sleep(5)
+    time.sleep(1)
     # submission to zocalo queue
 
     out = p1.communicate()[0]
-
-
     return out
-
-    # TODO:1: get the scipion directory from the recipie from the ['cwd'] instead of the command
-    # 2. If it is the top directory workout the paths to find the extra folder and look for the done.txt inside the folder for a fiven image
 
 
 if __name__ == '__main__':
     import os
 
-    user = os.environ.get('USER')
-    tmp_log_file = os.path.join('/dls/tmp/', user, 'zo_gctf_log.tmp')
+    extra_done_file = 'done_gctf.tmp'
+    extra_wd = run_zocalo_gctf()
 
-    with open(tmp_log_file, 'w') as tmplog:
-
-        tmplog.write("WE ARE STARTING ZO_GCTF\n")
-        tmplog.write("CWD is %s\n" % (os.getcwd()))
-        tmplog.flush()
-
-        # created in scipion project folder
-
-        extra_done_file = 'done.tmp'
-        tmplog.write("THIS IS '%s' \n" % (extra_done_file))
-
-        extra_wd = run_zocalo_gctf()
-
-        tmplog.write("extra_done file is " + extra_done_file + "\n")
-        while not os.path.exists(extra_done_file):
-            tmplog.write("waiting for '%s'\n" % os.path.realpath(extra_done_file))
-            tmplog.flush()
-            time.sleep(5)
-
-        tmplog.write("EXITING!!\n")
-        # remove the extra done file when program1
-
+    while not os.path.exists(extra_done_file):
+        time.sleep(5)
 
     if os.path.exists(extra_done_file):
         os.remove(extra_done_file)
         sys.exit(0)
-
-
-
-
